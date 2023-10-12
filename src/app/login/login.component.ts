1eec01438dac8e810ff9b43c58b945045779d701
Yuri <yuriro.rodriguez@gmail.com>
2023-10-12 09:06:37 -0500

    nueva actualizacion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit{
 
 
-  constructor(){
+  constructor( private router: Router){
 
   }
 
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit{
   }
 
   acceder(){
-
+    this.router.navigate(['/dashboard']);
   }
 
 }
