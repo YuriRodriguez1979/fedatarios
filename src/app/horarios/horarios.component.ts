b226a5cd75a3dfefae6e98e90e2c982475abeb82
Misantropo25 <019100178G@uandina.edu.pe>
2023-10-15 22:36:31 -0500

    Cambios en horarios y agregar usuario

--- src/app/horarios/horarios.component.ts
+++ src/app/horarios/horarios.component.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
+import { OnInit, AfterViewInit } from '@angular/core';
+import { Calendar } from '@fullcalendar/core'; // Importa Calendar
+import dayGridPlugin from '@fullcalendar/daygrid'; // Importa el plugin DayGrid
+import * as Pikaday from 'pikaday';
 
 @Component({
   selector: 'app-horarios',
   templateUrl: './horarios.component.html',
-  styleUrls: ['./horarios.component.css']
+  styleUrls: ['./horarios.component.css'],
 })
-export class HorariosComponent {
+export class HorariosComponent implements AfterViewInit {
+  calendar: Calendar | null = null;
+  nuevaFecha: string = ''; // Para almacenar la nueva fecha
+  eventoId: string | null = null; // Para almacenar el ID del evento a eliminar
+  nombreEvento: string = ''; // Variable para el nombre del evento
 
+  constructor() {}
+  ngAfterViewInit(): void {
+    const calendarEl = document.getElementById('calendar');
+
+    if (calendarEl) {
+      this.calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        events: [
+          {
+            id: '1',
+            title: 'Evento 1',
+            start: '2023-10-10',
+            color: 'green'
+          },
+          {
+            id: '2',
+            title: 'Evento 2',
+            start: '2023-10-15',
+            color: 'blue'
+          }
+        ],
+        eventClick: (info) => {
+          this.eventoId = info.event.id;
+        },
+      });
+      this.calendar.render();
+    }
+  }
+
+  abrirModal() {
+    // Abre el modal para agregar un evento
+    this.nombreEvento = ''; // Limpia el nombre del evento
+    document.getElementById('eventoModal')?.classList.add('show');
+  }
+  
+  agregarEvento(nuevaFecha: string) {
+    if (this.eventoId == '3'){
+      let numero = parseInt(this.eventoId , 10);
+      numero++;
+      this.eventoId = numero.toString();
+    }
+    if (this.calendar && nuevaFecha) {
+      this.calendar.addEvent({
+        id: this.eventoId?.toString(),
+        title: this.nombreEvento, // Utiliza el nombre del evento ingresado
+        start: nuevaFecha,
+        color: 'red' // Define el color del cuadro de fondo del nuevo evento
+      });
+      this.calendar.render();
+      this.nuevaFecha = '';
+      this.nombreEvento = ''; // Limpia el nombre del evento
+      document.getElementById('eventoModal')?.classList.remove('show'); // Cierra el modal
+    }
+  }
+
+  borrarEvento() {
+    if (this.calendar && this.eventoId) {
+      const evento = this.calendar.getEventById(this.eventoId);
+      if (evento) {
+        evento.remove();
+        this.eventoId = null;
+      }
+    }
+  }
 }
--- src/app/registro-usuarios/registro-usuarios.component.ts
+++ src/app/registro-usuarios/registro-usuarios.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // Agrega esta importación
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-usuarios',
   templateUrl: './registro-usuarios.component.html',
   styleUrls: ['./registro-usuarios.component.css']
 })
-export class RegistroUsuariosComponent {
+export class RegistroUsuariosComponent implements OnInit{
   
+  constructor( private router: Router){
+  }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
   onSubmit() {
     // Aquí puedes agregar la lógica para procesar el formulario, como enviar los datos al servidor.
+  }
+  acceder(){
+    this.router.navigate(['/login']);
   }
 }
 
