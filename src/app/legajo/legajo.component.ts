aa8dea94dac10ef546f228e89567561c116d3e27
YuriRodriguez1979 <yuriro.rodriguez@gmail.com>
2023-10-17 12:37:57 -0500

    ultima modificacion

--- src/app/legajo/legajo.component.ts
+++ src/app/legajo/legajo.component.ts
@@ -13,6 +13,9 @@ export class LegajoComponent implements OnInit {
   pipe: any
   fila:any
   bandera: boolean = false
+  habilitar: boolean = false
+  visibilidad: boolean = true
+
 
   constructor(){
 
@@ -25,18 +28,31 @@ export class LegajoComponent implements OnInit {
     let ChangedFormat = this.pipe.transform(this.hoy, 'dd/MM/YYYY');
     this.fecha = ChangedFormat;
 
-    $('#aumentar').click(function() {
-      $('#tabla>tbody').append('<tr><td><select class="form-control form-select-sm" aria-label="Small select example"><option selected>Tipo de Documento</option><option value="1">Resolucion</option><option value="2">Titulo</option><option value="3">Diplomas</option><option value="4">Maestria</option></select></td><td><div class="d-flex justify-content-between"><input class="form-control file" id="formFileLg" type="file">  <button type="button" class="btn btn-warning"><i class="fa-solid fa-trash"></i></button></div></td></tr>');
-    });
+    // $('#aumentar').click(function() {
+    //   $('#tabla>tbody').append('<tr><td><select class="form-control form-select-sm" aria-label="Small select example"><option selected>Tipo de Documento</option><option value="1">Resolucion</option><option value="2">Titulo</option><option value="3">Diplomas</option><option value="4">Maestria</option></select></td><td><div class="d-flex justify-content-between"><input class="form-control file" id="formFileLg" type="file">  <button type="button" class="btn btn-warning"><i class="fa-solid fa-trash"></i></button></div></td><td><input type="text" placeholder="Agregar URL" class="form-control"></td></tr>');
+    // });
 
   }
+  mostrarBotonTerminos = true; // Inicialmente visible
 
-  validar(valor: number){
+  validar(aceptado: number,valor: number) {
+    // Aquí puedes realizar alguna lógica de validación si es necesario
+    if (aceptado === 1) {
+      this.mostrarBotonTerminos = false; // Ocultar el botón
+    }
     if (valor > 0){
       this.bandera =  true
-
+      // $('#aumentar').click(function() {
+      //   $('#tabla>tbody').append('<tr><td><select class="form-control form-select-sm" aria-label="Small select example"><option selected>Tipo de Documento</option><option value="1">Resolucion</option><option value="2">Titulo</option><option value="3">Diplomas</option><option value="4">Maestria</option></select></td><td><div class="d-flex justify-content-between"><input class="form-control file" id="formFileLg" type="file">  <button type="button" class="btn btn-warning"><i class="fa-solid fa-trash"></i></button></div></td><td><input type="text" placeholder="Agregar URL" class="form-control"></td></tr>');
+      // });
     }
   }
 
+  aumentar(){
+    $('#aumentar').click(function() {
+      $('#tabla>tbody').append('<tr><td><select class="form-control form-select-sm" aria-label="Small select example"><option selected>Tipo de Documento</option><option value="1">Resolucion</option><option value="2">Titulo</option><option value="3">Diplomas</option><option value="4">Maestria</option></select></td><td><div class="d-flex justify-content-between"><input class="form-control file" id="formFileLg" type="file">  <button type="button" class="btn btn-warning"><i class="fa-solid fa-trash"></i></button></div></td><td><input type="text" placeholder="Agregar URL" class="form-control"></td></tr>');
+    });
+  }
 
 }
+
